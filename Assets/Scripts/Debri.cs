45391bd372c2cdf8068a1845aa27b951df2bcc9b
surumeica <dev.miyamoto.s@gmail.com>
2014-04-19 23:18:25 +0900

    練習中

---
+++ Assets/Scripts/Debri.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Debri : MonoBehaviour
+{
+		// 一秒あたりの回転角度
+		public float angle = 30f;
+		// 破壊時の得点
+		public int score = 10;
+		// 回転の中心座標
+		private Vector3 targetPos;
+		// Use this for initialization
+		void Start ()
+		{
+				Transform target = GameObject.Find ("Earth").transform;
+
+				targetPos = target.position;
+
+				transform.LookAt (target);
+
+				transform.Rotate (new Vector3 (0, 0, Random.Range (0, 360)), Space.World);
+		}
+		// Update is called once per frame
+		void Update ()
+		{
+				Vector3 axis = transform.InverseTransformDirection (Vector3.up);
+				transform.RotateAround (targetPos, axis, angle * Time.deltaTime);
+		}
+
+		void OnMouseDown ()
+		{
+				Destroy (gameObject);
+		}
+}
---
+++ Assets/Scripts/SpawnPoint.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class SpawnPoint : MonoBehaviour
+{
+		//宇宙ゴミ　プレハブ
+		public GameObject debri;
+		// 宇宙ゴミ発生感覚
+		public float interval = 1f;
+		// Use this for initialization
+		void Start ()
+		{
+				//SpawnDebris() コルーチンを開始する
+				StartCoroutine ("SpawnDebris");
+		}
+
+		IEnumerator SpawnDebris ()
+		{
+				//無限ループ
+				while (true) {
+						//宇宙ゴミプレハブを SpawnPointオブジェクトの位置にインスタンス化する
+						Instantiate (debri, transform.position, Quaternion.identity);
+						// interval 分だけ処理を停止する
+						yield return new WaitForSeconds (interval);
+				}
+		}
+		// Update is called once per frame
+		void Update ()
+		{
+	
+		}
+}
